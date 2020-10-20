3694fdcf0e28b69856bded382cfbd1f01029bffb
Kacper Zaleski <zal19114@spengergasse.at>
2020-10-20 13:05:55 +0200

    add db connection

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const routes = require("./routes/index");
 
 const app = express();
 
+const mongoose = require("mongoose");
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -15,5 +17,10 @@ app.use(express.static(path.join(__dirname, "./client/build")));
 
 app.use("/", routes);
 app.use("/api/data", routes);
+
+mongoose.connect(
+  "mongodb+srv://admin:admin@challenge-me.nalk9.mongodb.net/challenge-me?retryWrites=true&w=majority",
+  { useNewUrlParser: true }
+);
 
 module.exports = app;
