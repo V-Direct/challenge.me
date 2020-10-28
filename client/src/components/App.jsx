74c9c32ed7cf6edbbc62bb6f885f671a9d410098
Kacper Zaleski <zal19114@spengergasse.at>
2020-10-28 12:27:26 +0100

    add dockerfile

---
+++ client/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "../css/App.css";
+import NavBar from "./NavBar";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
