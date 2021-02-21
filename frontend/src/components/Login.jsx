7c94ea3a9b287008d7da5b410bf666fe01d73cae
Kacper Zaleski <ZAL19114@spengergasse.at>
2021-02-21 15:21:17 +0100

    cleanup after merge #2

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
 
-export default function Login() {
+async function loginUser(username, password, props){
+    const response= await fetch('http://localhost:3001/user/'+ username,{
+        method: 'GET',
+        headers: { 
+            'Accept' : 'application/json',
+            'Content-Type': 'application/json' 
+        }       
+    });
+    const data = await response.json()
+
+    if(data.password !== password){
+        document.getElementById("errorLabel").innerHTML='Falsches passwort'
+    }
+}
+
+
+
+function errorLabel() {
+    const username = document.getElementById("inputUsername").value;
+    const password = document.getElementById("inputPassword").value;
+    document.getElementById("errorLabel").innerHTML=''
+
+    if(username.length === 0 || password.length === 0) {
+        document.getElementById("errorLabel").innerHTML='Fehler'
+    }
+    else{
+        loginUser(username,password)
+    }
+    
+}
+
+ function Login() {
     return (
-        <div>
-            
+        <div>                 
+                    <br></br>
+                    <ol><label>username</label></ol>
+                    <ol><input id="inputUsername"></input></ol>
+                    <ol><label>Password</label></ol>
+                    <ol> <input id="inputPassword"></input></ol>
+                    <ol><label id="errorLabel"></label></ol>
+                    <ol> <button onClick={errorLabel}>Login</button></ol>
+                    
         </div>
     )
 }
+
+export default Login
