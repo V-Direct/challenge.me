90e00abc956c6308cb1b1265a9c93fd9dfd4d0d2
Kacper Zaleski <ZAL19114@spengergasse.at>
2021-02-21 14:17:00 +0100

    continue challenge component + styling

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -3,16 +3,66 @@ import "../css/App.css";
 import NavBar from "./NavBar/NavBar";
 import ChallengeItem from "./ChallengeItem/ChallengeItem";
 
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      data: []
+    }
+  }
+    
+  componentDidMount(){
+      fetch("http://localhost:3001/challenge/")
+        .then((res) => res.json())
+        .then((challenge) => {
+          this.setState({data: challenge});
+          console.log(this.state);
+        });
+  }
 
-function App() {
-  return (
-    <div>
-      <NavBar />
-      <div>
-        <ChallengeItem></ChallengeItem>
-      </div>
+  render(){
+    const {data} = this.state
+    return (
+      <div className="app">
+        <NavBar />
+        {data.map(item => (
+          <ChallengeItem 
+            key={item._id}
+            title={item.title}
+            tag={item.tag}
+            description={item.description}
+            creator={item.creator}
+            labels={item.labels}
+          />
+        ))}
     </div>
-  );
+    )
+  }
+  
 }
 
 export default App;
+
+// function App() {
+//   const [state, setState] = useState([]);
+
+//   useEffect(() => {
+//     fetch("http://localhost:3001/challenge/")
+//       .then((res) => res.json())
+//       .then((data) => setState(data));
+//   });
+
+//   return (
+//     <div>
+//       <NavBar />
+//       {state.map((item) => (
+//         <ChallengeItem
+//           title={item.title}
+//           tag={item.tag}
+//           description={item.description}
+//           labels={item.labels}
+//         />
+//       ))}
+//     </div>
+//   );
+// }
--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import NavBarItem from "./NavBarItem";
 import "../../css/NavBar.css";
+import "materialize-css"
 
 function NavBar() {
   return (
-    <nav className="navitem">
-      <NavBarItem content="Home" />
-      <NavBarItem content="Contact" />
-      <NavBarItem content="Register" />
+    <nav className="">
+      <div className="nav-wrapper blue darken-3">
+        <NavBarItem content="Home" link=""/>
+        <NavBarItem content="Contact" link="contact"/>
+        <NavBarItem content="Register" link="register"/>
+      </div>
     </nav>
   );
 }
