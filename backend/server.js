c1643ff2185df150fc5fa700ff55de138b88c7b3
Zeljko Predjeskovic <pre18309@spengergasse.at>
2021-03-28 18:55:54 +0200

    findCreatorChallenges

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+//app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 let allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', "*");
