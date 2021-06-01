be75f9e0ebc36def7a706c071673980d90c76438
Zeljko Predjeskovic <zepr2022000@gmail.com>
2021-06-01 12:11:20 +0200

    added challengeview table

--- frontend/src/components/ChallengeView/ChallengeView.jsx
+++ frontend/src/components/ChallengeView/ChallengeView.jsx
@@ -2,6 +2,24 @@ import React from "react";
 
 export default class ChallengeView extends React.Component {
   render() {
-    return <div></div>;
+    return <div>
+    <table className="responsive-table white-text text-darken-2">
+        <thead>
+          <tr>
+              <th>Title</th>
+              <th>Tag</th>
+              <th>Description</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>{this.props.location.challenge.title}</td>
+            <td>{this.props.location.challenge.tag}</td>
+            <td>{this.props.location.challenge.description}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>;
   }
 }
